7dfc8e767c7f96a779815a75e006ecb02bae9674
James Montealegre <83414986+JamesMontealegre@users.noreply.github.com>
2024-10-09 02:02:26 -0500

    feature(access): include login view and i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HttpClient } from '@angular/common/http'; // Asegúrate de importar HttpClientModule
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
