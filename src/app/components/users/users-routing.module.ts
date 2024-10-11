0e899f45b48dd4b80d168fa2a057cc4ce10c2dcd
James Montealegre <83414986+JamesMontealegre@users.noreply.github.com>
2024-10-10 22:53:36 -0500

    feature(*): add new components and menu navigation

--- src/app/components/users/users-routing.module.ts
+++ src/app/components/users/users-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/components/users/users.module.ts
+++ src/app/components/users/users.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { UsersFormComponent } from './components/users-form/users-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsersListComponent,
+    UsersFormComponent
+  ],
   imports: [
     CommonModule,
     UsersRoutingModule
