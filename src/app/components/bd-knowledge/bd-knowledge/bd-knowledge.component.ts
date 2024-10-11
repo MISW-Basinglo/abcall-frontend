eb3fe8035ff1de5eaef441f503af382577f645f2
James Montealegre <83414986+JamesMontealegre@users.noreply.github.com>
2024-10-11 10:24:34 -0500

    feature(access): include other components within menu and  i18n

---
+++ src/app/components/bd-knowledge/bd-knowledge/bd-knowledge.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bd-knowledge',
+  templateUrl: './bd-knowledge.component.html',
+  styleUrls: ['./bd-knowledge.component.scss']
+})
+export class BdKnowledgeComponent {
+
+}
--- src/app/components/dashboard/dashboard-routing.module.ts
+++ src/app/components/dashboard/dashboard-routing.module.ts
@@ -4,6 +4,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { IncidentsListComponent } from '../incidents/components/incidents-list/incidents-list.component';
 import { UsersListComponent } from '../users/components/users-list/users-list.component';
 import { ReportComponent } from '../reports/report/report.component';
+import { ProfileComponent } from '../profile/profile/profile.component';
+import { BdKnowledgeComponent } from '../bd-knowledge/bd-knowledge/bd-knowledge.component';
+import { ClientsComponent } from '../clients/clients/clients.component';
 
 const routes: Routes = [
   {
@@ -13,6 +16,9 @@ const routes: Routes = [
       { path: 'incidents', component: IncidentsListComponent },
       { path: 'users', component: UsersListComponent },
       { path: 'reports', component: ReportComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'bd', component: BdKnowledgeComponent },
+      { path: 'clients', component: ClientsComponent },
       { path: '', redirectTo: 'incidents', pathMatch: 'full' },
     ],
   },
