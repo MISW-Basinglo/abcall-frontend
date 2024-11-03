cc4fe0d144bb17aeba7a4ef9139ec752a902febb
Richard Acevedo <richard.acevedo.ext@inetum.com>
2024-11-03 17:12:24 -0500

    Fix: fix environment

--- src/app/components/auth/services/auth/auth.service.ts
+++ src/app/components/auth/services/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
